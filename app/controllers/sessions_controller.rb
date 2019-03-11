ea2fa74b18ae0beaeb8ce260bbec18769d4bc197
Milos <buc.milos@gmail.com>
2019-03-11 08:50:33 +0100

    Finish user edit, update, index, and destroy actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,22 +1,21 @@
 class SessionsController < ApplicationController
+
   def new
   end
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-    if ?user && ?user.authenticate(params[:session][:password])
-      log_in ?user
-      params[:session][:remember_me] == '1' ? remember(?user) : forget(?user)
-      redirect_to ?user
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_back_or user
     else
-      flash.now[:danger] = 'Invalid email/password combination'
+      flash.now[:error] = 'Invalid email/password combination'
       render 'new'
     end
   end
 
   def destroy
-    log_out if logged_in?
+    log_out
     redirect_to root_url
-
   end
 end
--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -6,7 +6,15 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     @user = users(:michael)
   end
 
-  
+  test "login with invalid information" do
+    get login_path
+    assert_template 'sessions/new'
+    post login_path, params: { session: { email: "", password: "" } }
+    assert_template 'sessions/new'
+    assert_not flash.empty?
+    get root_path
+    assert flash.empty?
+end
 
   test "login with valid information followed by logout" do
     get login_path
@@ -32,15 +40,12 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
 
   test "login with remembering" do
     log_in_as(@user, remember_me: '1')
-    assert_equal FILL_IN, assigns(:user).FILL_IN
+    #assert_equal FILL_IN, assigns(:user).FILL_IN
+    assert_not_nil cookies['remember_token']
   end
 
   test "login without remembering" do
-    # Log in to set the cookie.
-    log_in_as(@user, remember_me: '1')
-    # Log in again and verify that the cookie is deleted.
-  
     log_in_as(@user, remember_me: '0')
-    assert_empty cookies[:remember_token]
-  end
+    assert_nil cookies['remember_token']
+    end
 end
