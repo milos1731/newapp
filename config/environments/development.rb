7016f399437df51044457c7bd195f02191a1f539
Milos <buc.milos@gmail.com>
2019-03-12 10:39:45 +0100

    Add account activation

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,10 +31,11 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
-
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :test
   config.action_mailer.perform_caching = false
-
+  host = 'https://aqueous-cliffs-59418.herokuapp.com'
+  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
